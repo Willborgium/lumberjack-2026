99778c397feadeee211bf2e04e26aad55f46bb21
Will Custode <wjcustode@gmail.com>
2026-01-23 23:54:21 -0500

    Added shapes

--- Driver.cs
+++ Driver.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Lumberjack;
 
@@ -8,7 +9,8 @@ public class Driver : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
-    private Renderable3D _cubeRenderable;
+    private List<Renderable3D> _renderables = new List<Renderable3D>();
+    private List<Matrix> _baseWorlds = new List<Matrix>();
     private float _angle;
 
     public Driver()
@@ -28,9 +30,39 @@ public class Driver : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // create a simple cube renderable from test data
-        TestFunctions.CreateCube(out var verts, out var inds);
-        _cubeRenderable = new Renderable3D(GraphicsDevice, verts, inds);
+        // create four renderables: cube, sphere, pyramid, rectangular prism
+        TestFunctions.CreateCube(out var cubeVerts, out var cubeInds);
+        var cube = new Renderable3D(GraphicsDevice, cubeVerts, cubeInds);
+
+        TestFunctions.CreateSphere(out var sphereVerts, out var sphereInds, stacks: 10, slices: 14, radius: 0.9f);
+        var sphere = new Renderable3D(GraphicsDevice, sphereVerts, sphereInds);
+
+        TestFunctions.CreatePyramid(out var pyramidVerts, out var pyramidInds, size: 0.9f, height: 1.4f);
+        var pyramid = new Renderable3D(GraphicsDevice, pyramidVerts, pyramidInds);
+
+        TestFunctions.CreateRectangularPrism(out var prismVerts, out var prismInds, width: 1.6f, height: 0.8f, depth: 0.6f);
+        var prism = new Renderable3D(GraphicsDevice, prismVerts, prismInds);
+
+        // position them in the scene and record base transforms
+        var cubeBase = Matrix.CreateTranslation(new Vector3(-2.2f, 0f, 0f));
+        var sphereBase = Matrix.CreateTranslation(new Vector3(2.2f, 0f, 0f));
+        var pyramidBase = Matrix.CreateTranslation(new Vector3(0f, 1.6f, 0f));
+        var prismBase = Matrix.CreateTranslation(new Vector3(0f, -1.6f, 0f));
+
+        cube.World = cubeBase;
+        sphere.World = sphereBase;
+        pyramid.World = pyramidBase;
+        prism.World = prismBase;
+
+        _renderables.Add(cube);
+        _renderables.Add(sphere);
+        _renderables.Add(pyramid);
+        _renderables.Add(prism);
+
+        _baseWorlds.Add(cubeBase);
+        _baseWorlds.Add(sphereBase);
+        _baseWorlds.Add(pyramidBase);
+        _baseWorlds.Add(prismBase);
     }
 
     protected override void Update(GameTime gameTime)
@@ -48,15 +80,23 @@ public class Driver : Game
     {
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        if (_cubeRenderable != null)
+        if (_renderables != null && _renderables.Count > 0)
         {
             GraphicsDevice.DepthStencilState = DepthStencilState.Default;
             GraphicsDevice.RasterizerState = new RasterizerState { CullMode = CullMode.CullCounterClockwiseFace };
 
-            _cubeRenderable.World = Matrix.CreateRotationY(_angle) * Matrix.CreateRotationX(_angle * 0.6f);
-            var view = Matrix.CreateLookAt(new Vector3(0, 0, 5f), Vector3.Zero, Vector3.Up);
+            var view = Matrix.CreateLookAt(new Vector3(0, 0, 6f), Vector3.Zero, Vector3.Up);
             var proj = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45f), GraphicsDevice.Viewport.AspectRatio, 0.1f, 100f);
-            _cubeRenderable.Draw(GraphicsDevice, view, proj);
+
+            // draw each with a small rotation
+            for (int i = 0; i < _renderables.Count; i++)
+            {
+                var r = _renderables[i];
+                var baseWorld = _baseWorlds[i];
+                var rot = Matrix.CreateRotationY(_angle * (1f + i * 0.15f)) * Matrix.CreateRotationX(_angle * 0.5f);
+                r.World = rot * baseWorld;
+                r.Draw(GraphicsDevice, view, proj);
+            }
         }
 
         base.Draw(gameTime);
--- TestFunctions.cs
+++ TestFunctions.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
@@ -27,5 +28,107 @@ public static class TestFunctions
             3,2,6, 3,6,7,
             4,5,1, 4,1,0
         };
+    }
+
+    public static void CreateSphere(out VertexPositionColor[] vertices, out short[] indices, int stacks = 8, int slices = 12, float radius = 1f)
+    {
+        var verts = new System.Collections.Generic.List<VertexPositionColor>();
+        var inds = new System.Collections.Generic.List<short>();
+
+        for (int i = 0; i <= stacks; i++)
+        {
+            float v = i / (float)stacks;
+            float theta = v * MathF.PI;
+
+            for (int j = 0; j <= slices; j++)
+            {
+                float u = j / (float)slices;
+                float phi = u * MathF.PI * 2f;
+                
+                float x = MathF.Sin(theta) * MathF.Cos(phi);
+                float y = MathF.Cos(theta);
+                float z = MathF.Sin(theta) * MathF.Sin(phi);
+
+                var pos = new Microsoft.Xna.Framework.Vector3(x, y, z) * radius;
+                // color by normal
+                var col = new Microsoft.Xna.Framework.Color((x + 1f) * 0.5f, (y + 1f) * 0.5f, (z + 1f) * 0.5f);
+                verts.Add(new VertexPositionColor(pos, col));
+            }
+        }
+
+        int ring = slices + 1;
+        for (int i = 0; i < stacks; i++)
+        {
+            for (int j = 0; j < slices; j++)
+            {
+                short first = (short)(i * ring + j);
+                short second = (short)((i + 1) * ring + j);
+
+                inds.Add(first);
+                inds.Add(second);
+                inds.Add((short)(first + 1));
+
+                inds.Add(second);
+                inds.Add((short)(second + 1));
+                inds.Add((short)(first + 1));
+            }
+        }
+
+        vertices = verts.ToArray();
+        indices = inds.ToArray();
+    }
+
+    public static void CreatePyramid(out VertexPositionColor[] vertices, out short[] indices, float size = 1f, float height = 1.2f)
+    {
+        float s = size;
+        var v = new VertexPositionColor[5];
+        v[0] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(-s, 0, -s), Microsoft.Xna.Framework.Color.Red);
+        v[1] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(s, 0, -s), Microsoft.Xna.Framework.Color.Green);
+        v[2] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(s, 0, s), Microsoft.Xna.Framework.Color.Blue);
+        v[3] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(-s, 0, s), Microsoft.Xna.Framework.Color.Yellow);
+        v[4] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(0, height, 0), Microsoft.Xna.Framework.Color.White);
+
+        // base two triangles + 4 side triangles
+        var idx = new short[]
+        {
+            0,1,2, 0,2,3,
+            0,1,4,
+            1,2,4,
+            2,3,4,
+            3,0,4
+        };
+
+        vertices = v;
+        indices = idx;
+    }
+
+    public static void CreateRectangularPrism(out VertexPositionColor[] vertices, out short[] indices, float width = 1.5f, float height = 0.8f, float depth = 0.6f)
+    {
+        float hw = width * 0.5f;
+        float hh = height * 0.5f;
+        float hd = depth * 0.5f;
+
+        var v = new VertexPositionColor[8];
+        v[0] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(-hw, -hh, -hd), Microsoft.Xna.Framework.Color.CornflowerBlue);
+        v[1] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(hw, -hh, -hd), Microsoft.Xna.Framework.Color.CadetBlue);
+        v[2] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(hw, hh, -hd), Microsoft.Xna.Framework.Color.LightGreen);
+        v[3] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(-hw, hh, -hd), Microsoft.Xna.Framework.Color.Gold);
+        v[4] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(-hw, -hh, hd), Microsoft.Xna.Framework.Color.OrangeRed);
+        v[5] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(hw, -hh, hd), Microsoft.Xna.Framework.Color.MediumPurple);
+        v[6] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(hw, hh, hd), Microsoft.Xna.Framework.Color.White);
+        v[7] = new VertexPositionColor(new Microsoft.Xna.Framework.Vector3(-hw, hh, hd), Microsoft.Xna.Framework.Color.PaleVioletRed);
+
+        var idx = new short[]
+        {
+            0,1,2, 0,2,3,
+            4,6,5, 4,7,6,
+            4,3,7, 4,0,3,
+            1,5,6, 1,6,2,
+            3,2,6, 3,6,7,
+            4,5,1, 4,1,0
+        };
+
+        vertices = v;
+        indices = idx;
     }
 }
